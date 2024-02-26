8e34bb6e74ca0612a4a8ec0928f88cc6a2bf32b2
olabanji lateef <olabanjilateef1@gmail.com>
2024-02-26 13:28:29 +0100

    second push

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -11,7 +11,7 @@ const Message = ({ message }) => {
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
 	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-	const shakeClass = message.shouldShake ? "shake" : "";
+	const shakeClass = message.shouldShake ? "shake" : ""; // comment
 
 	return (
 		<div className={`chat ${chatClassName}`}>
